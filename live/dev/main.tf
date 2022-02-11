e6c052f3dd02c4c59a97099f0f5f3d304c9942bf
ravindrasinghmutualmobile <ravindra.singh@mutualmobile.com>
2022-02-11 11:38:26 +0530

    added terraform initital config and first resource

---
+++ live/dev/main.tf
@@ -0,0 +1,27 @@
+#HCL Below
+#Block type is written at the beggining 
+# block type can be resource, data, output, variable, module etc.
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "LearnTerraform"
+  }
+}
